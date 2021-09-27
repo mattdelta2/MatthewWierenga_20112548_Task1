7e7c6e3ae229b036c60792015f31d2b62ddc5899
mattw <mattw@MATTS-PC>
2021-09-27 12:38:10 +0200

    Add project files.

---
+++ MatthewWierenga_20112548_Task1/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatthewWierenga_20112548_Task1
+{
+    class Character
+    {
+        public int characterMaxHP;
+        public int characterDamage;
+        public int characterHP;
+
+        public int GoblinMaxHP;
+        public int GoblinDamage;
+        public int GoblinHP;
+
+        public enum Movement
+        {
+            NoMovement,
+            Up,
+            Down,
+            Right,
+            Left,
+            }
+
+    }
+}
---
+++ MatthewWierenga_20112548_Task1/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatthewWierenga_20112548_Task1
+{
+    class Tile
+    {
+        public int x;
+        public int y;
+        public enum tileType
+        {
+            Hero,
+            Gold,
+            Enemy,
+        }
+
+
+
+    }
+
+}
